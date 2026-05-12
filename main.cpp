3d7cfe827997454b0b0b4e27a33a3d9bc0f917e0
Shanjida Islam <shanjida.islam9938@gmail.com>
2026-05-12 13:14:33 +0600

    10th commit

--- main.cpp
+++ main.cpp
@@ -3,37 +3,71 @@
 #include "menu.h"
 #include "screens.h"
 #include "input.h"
+#include "game.h"
 
-int gameState = 0;
+int gameState=0;
 
 void display(){
+
     glClear(GL_COLOR_BUFFER_BIT);
 
     drawBackground();
 
-    if(gameState == 0) drawMenu();
-    else if(gameState == 1) drawGameScreen();
-    else if(gameState == 2) drawLevelScreen();
+    if(gameState==0)
+        drawMenu();
+
+    else if(gameState==1){
+
+        if(lives<=0)
+            drawGameOver();
+
+        else
+            drawGameScreen();
+    }
+
+    else if(gameState==2)
+        drawLevelScreen();
 
     glFlush();
 }
 
+void timer(int v){
+
+    if(gameState==1 && lives>0){
+        updateGame();
+    }
+
+    glutPostRedisplay();
+    glutTimerFunc(16,timer,0);
+}
+
 void init(){
+
     glClearColor(0,0,0,1);
+
+    glMatrixMode(GL_PROJECTION);
     gluOrtho2D(0,800,0,600);
 }
 
 int main(int argc,char** argv){
 
     glutInit(&argc,argv);
+
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
     glutInitWindowSize(800,600);
-    glutCreateWindow("DX BALL UI DEMO");
+
+    glutCreateWindow("DX BALL FULL PROJECT");
 
     init();
 
     glutDisplayFunc(display);
+
     glutKeyboardFunc(handleInput);
+
+    glutSpecialFunc(handleSpecial);
+
+    glutTimerFunc(0,timer,0);
 
     glutMainLoop();
 }
